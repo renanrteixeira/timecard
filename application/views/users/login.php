043eab50b44d737daafdbf6aaa7d1628cd4a77d8
Renan Teixeira <renan.r.teixeira@gmail.com>
2019-08-18 10:36:58 -0300

    correções posicionamento e div login.php

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -15,7 +15,11 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 	<link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
 	<!-- Bootstrap 3.3.7 -->
-	<link rel="stylesheet" href="<?php echo base_url('/includes/bootstrap/css/bootstrap.min.css') ?>">
+	<link rel="stylesheet" href="<?php echo base_url('/assets/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>">
+    <!-- Ionicons -->
+	<link rel="stylesheet" href="<?php echo base_url('/assets/bower_components/Ionicons/css/ionicons.min.css') ?>">	
+    <!-- iCheck -->
+	<link rel="stylesheet" href="<?php echo base_url('/assets/plugins/iCheck/square/blue.css') ?>">
 	<!--Fontawesome CDN-->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 	<!-- Theme style -->
@@ -24,44 +28,45 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<link href="<?php echo base_url('/includes/login/login.css') ?>" rel="stylesheet">
 </head>
 
-<body class="text-center hold-transition login-page">
+<body class="hold-transition login-page text-center">
 	<div class="login-box">
-		<?php echo form_open('Users/doLogin'); ?>
-		<form>
-			<img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-			<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-			<?php if (validation_errors()) { ?>
-			<div class="callout callout-danger">
-				<h4>Warning!</h4>
-				<p><?php echo validation_errors(); ?></p>
-			</div>
-			<?php } ?>
-			<?php if (!empty($this->input->get('msg')) && $this->input->get('msg') == 1) { ?>
-			<div class="callout callout-danger">
-				E-mail e/ou senha inválidos!
-			</div>
-			<?php } ?>
-			<div class="input-group form-group">
-				<div class="input-group-prepend">
-					<span class="input-group-text"><i class="fas fa-user"></i></span>
+		<div class="login-logo">
+			<a href="#"><b>Controle</b>Ponto</a>
+		</div>		
+		<div class="login-box-body">
+			<?php echo form_open('Users/doLogin'); ?>
+			<form>
+				<img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+				<h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+				<?php if (validation_errors()) { ?>
+				<div class="callout callout-danger">
+					<h4>Warning!</h4>
+					<p><?php echo validation_errors(); ?></p>
 				</div>
-				<input type="email" id="email" name="email" class="form-control" placeholder="E-mail address" required autofocus>
-			</div>
-			<div class="input-group form-group">
-				<div class="input-group-prepend">
-					<span class="input-group-text"><i class="fas fa-key"></i></span>
+				<?php } ?>
+				<?php if (!empty($this->input->get('msg')) && $this->input->get('msg') == 1) { ?>
+				<div class="callout callout-danger">
+					E-mail e/ou senha inválidos!
 				</div>
-				<input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
-			</div>
-			<div class="checkbox mb-3">
-				<label>
-					<input type="checkbox" value="remember-me"> Remember me
-				</label>
-			</div>
-			<button class="btn btn-lg btn-primary btn-block" type="submit" id="Login">Sign in</button>
-			<p class="mt-5 mb-3 text-muted">&copy; 2019</p>
-		</form>
-		<?php echo form_close(); ?>
+				<?php } ?>
+				<div class="form-group has-feedback">
+					<input type="email" id="email" name="email" class="form-control" placeholder="E-mail address" required autofocus>
+					<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+				</div>			
+				<div class="form-group has-feedback">
+					<input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+					<span class="glyphicon glyphicon-lock form-control-feedback"></span>
+				</div>
+				<div class="checkbox mb-3">
+					<label>
+						<input type="checkbox" value="remember-me"> Remember me
+					</label>
+				</div>
+				<button class="btn btn-lg btn-primary btn-block" type="submit" id="Login">Sign in</button>
+				<p class="mt-5 mb-3 text-muted">&copy; 2019</p>
+			</form>
+			<?php echo form_close(); ?>
+		</div>
 	</div>
 	<!-- jQuery 3 -->
 	<script src="<?php echo base_url('/assets/bower_components/jquery/dist/jquery.min.js') ?>"></script>
@@ -73,7 +78,17 @@ defined('BASEPATH') or exit('No direct script access allowed');
 	<script src="<?php echo base_url('/assets/dist/js/adminlte.min.js') ?>"></script>
 	<!-- AdminLTE for demo purposes -->
 	<script src="<?php echo base_url('/assets/dist/js/demo.js') ?>"></script>
-
+	<!-- iCheck -->
+	<script src="<?php echo base_url('/assets/plugins/iCheck/icheck.min.js') ?>"></script>
+	<script>
+	$(function () {
+		$('input').iCheck({
+		checkboxClass: 'icheckbox_square-blue',
+		radioClass: 'iradio_square-blue',
+		increaseArea: '20%' /* optional */
+		});
+	});
+	</script>	
 </body>
 
 </html>
