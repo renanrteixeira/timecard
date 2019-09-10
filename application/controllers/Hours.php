2f70e97f3d583c100def47217e5dadf15d7426fe
Renan Teixeira <proton_renan@hotmail.com>
2019-09-10 08:26:58 -0300

    correção de cálculo da hora negativa

--- application/controllers/Hours.php
+++ application/controllers/Hours.php
@@ -184,7 +184,7 @@ class Hours extends CI_Controller {
 					$Saidaextra   = $this->input->post('hour6').':00';
 				}
 
-
+/*
 				$balance = ((strtotime($Saidamanha) - strtotime($Entradamanha))+
 						    (strtotime($Saidatarde) - strtotime($Entradatarde))+
 						    (strtotime($Saidaextra) - strtotime($Entradaextra)));
@@ -199,8 +199,8 @@ class Hours extends CI_Controller {
 					$balance = strtotime($balance) - strtotime($hourbase);
 					$balance = date('H:i:s', $balance);
 				}
+			*/
 			
-/*			
 			  
 			    $h1 = $this->toUnixTime($Entradamanha);
 			    $h2 = $this->toUnixTime($Saidamanha);
@@ -215,9 +215,19 @@ class Hours extends CI_Controller {
 
      			$balance = $this->getFullHour($balance);
 				
-				$balance = $this->toUnixTime($balance) - $this->toUnixTime($hourbase);
+                if ($this->toUnixTime($balance) < $this->toUnixTime($hourbase)) {
 
-				$balance = $this->getFullHour($balance);*/
+					$balance = $this->toUnixTime($hourbase) - $this->toUnixTime($balance);
+
+					$balance = $this->getFullHour($balance);
+					
+					$balance = '-'.$balance;
+	
+				} else {
+					$balance = $this->toUnixTime($balance) - $this->toUnixTime($hourbase);
+
+					$balance = $this->getFullHour($balance);
+				}
 
 				//echo  'Base: '.$hourbase.'  -  Convertido: '.strtotime($hourbase).'  -  Entrada: '.$Entradamanha.'  -  Convertido: '.strtotime($Entradamanha).' Balance: '.$balance;
 				$dados = array(
